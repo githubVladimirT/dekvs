60bf015ae4240f09420ec2934a9dd98a5306b23d
Vladimir Tolstov <vladimir.tolstov@selfip.ru>
2026-01-13 20:49:55 +0300

    ...
    Signed-off-by: Vladimir Tolstov <vladimir.tolstov@selfip.ru>

--- src/main.go
+++ src/main.go
@@ -1,1 +1,100 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	httpd "github.com/githubVladimirT/dekvs/src/http"
+	"github.com/githubVladimirT/dekvs/src/store"
+)
+
+const (
+	DefaultHTTPAddr = "localhost:8080"
+	DefaultRaftAddr = "localhost:8000"
+)
+
+var (
+	inMemory bool
+	httpAddr string
+	raftAddr string
+	joinAddr string
+	nodeID   string
+)
+
+func init() {
+	flag.BoolVar(&inMemory, "in_memory", false, "Use in-memory storage for Raft")
+	flag.StringVar(&httpAddr, "http_addr", DefaultHTTPAddr, "Set the HTTP bind address")
+	flag.StringVar(&raftAddr, "raft_addr", DefaultRaftAddr, "Set Raft bind address")
+	flag.StringVar(&joinAddr, "join", "", "Set join address, if any")
+	flag.StringVar(&nodeID, "id", "", "Node ID. If not set, same as Raft bind address")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <raft-data-path> \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "No Raft storage directory specified\n")
+		os.Exit(-1)
+	}
+
+	if nodeID == "" {
+		nodeID = raftAddr
+	}
+
+	raftDir := flag.Arg(0)
+	if raftDir == "" {
+		log.Fatalln("No Raft storage directory specified")
+	}
+
+	if err := os.MkdirAll(raftDir, 0o700); err != nil {
+		log.Fatalf("failed to create path for Raft storage: %s", err.Error())
+	}
+
+	s := store.New(inMemory)
+	s.RaftDir = raftDir
+	s.RaftBind = raftAddr
+
+	if err := s.Open(joinAddr == "", nodeID); err != nil {
+		log.Fatalf("failed to open store: %s", err.Error())
+	}
+
+	h := httpd.New(httpAddr, s)
+	if err := h.Start(); err != nil {
+		log.Fatalf("failed to start HTTP service: %s", err.Error())
+	}
+
+	if joinAddr != "" {
+		if err := join(joinAddr, raftAddr, nodeID); err != nil {
+			log.Fatalf("failed to join node at %s: %s", joinAddr, err.Error())
+		}
+	}
+
+	log.Printf("dekvs started successfully, listening on http://%s", httpAddr)
+
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, os.Interrupt)
+	<-terminate
+	log.Println("dekvs exiting")
+}
+
+func join(joinAddr, raftAddr, nodeID string) error {
+	b, err := json.Marshal(map[string]string{"addr": raftAddr, "id": nodeID})
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(fmt.Sprintf("http://%s/join", joinAddr), "application-type/json", bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
