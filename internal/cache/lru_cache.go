cf3d53a90cecd60f3fb0838c782b2979a2cd51ac
Vladimir Tolstov <vladimir.tolstov@selfip.ru>
2026-02-06 17:52:00 +0300

    added lru cache implementation
    Signed-off-by: Vladimir Tolstov <vladimir.tolstov@selfip.ru>

--- internal/cache/lru_cache.go
+++ internal/cache/lru_cache.go
@@ -1,1 +1,96 @@
 package cache
+
+import (
+	"container/list"
+	"sync"
+)
+
+type Cache interface {
+	Get(key string) (interface{}, bool)
+	Put(key string, value interface{})
+	Delete(key string)
+	Clear()
+	Len() int
+}
+
+type LRUCache struct {
+	capacity int
+	cache    map[string]*list.Element
+	list     *list.List
+	mu       sync.RWMutex
+}
+
+type entry struct {
+	key   string
+	value interface{}
+}
+
+func NewLRUCache(capacity int) *LRUCache {
+	return &LRUCache{
+		capacity: capacity,
+		cache:    make(map[string]*list.Element),
+		list:     list.New(),
+	}
+}
+
+func (c *LRUCache) Get(key string) (interface{}, bool) {
+	c.mu.RLock()
+	defer c.mu.Unlock()
+
+	if el, ok := c.cache[key]; ok {
+		c.list.MoveToFront(el)
+		return el.Value.(*entry).value, true
+	}
+	return nil, false
+}
+
+func (c *LRUCache) Put(key string, value interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if el, ok := c.cache[key]; ok {
+		c.list.MoveToFront(el)
+		el.Value.(*entry).value = value
+		return
+	}
+
+	el := c.list.PushFront(&entry{key, value})
+	c.cache[key] = el
+
+	if c.list.Len() > c.capacity {
+		c.removeOldest()
+	}
+}
+
+func (c *LRUCache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if el, ok := c.cache[key]; ok {
+		c.list.Remove(el)
+		delete(c.cache, key)
+	}
+}
+
+func (c *LRUCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache = make(map[string]*list.Element)
+	c.list.Init()
+}
+
+func (c *LRUCache) Len() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return c.list.Len()
+}
+
+func (c *LRUCache) removeOldest() {
+	el := c.list.Back()
+	if el != nil {
+		c.list.Remove(el)
+		delete(c.cache, el.Value.(*entry).key)
+	}
+}
