1f948a96289da23cf4677ca4b7b5c41bff69f48c
Vladimir Tolstov <vladimir.tolstov@selfip.ru>
2026-02-10 11:36:54 +0300

    redoing...
    Signed-off-by: Vladimir Tolstov <vladimir.tolstov@selfip.ru>

--- internal/raft/fsm.go
+++
@@ -1,9 +0,0 @@
-package raft
-
-import "github.com/githubVladimirT/dekvs/internal/store"
-
-type FSMManager interface {
-	Get(key string) ([]byte, uint64, bool)
-	GetVersionHistory(key string, limit int) []*store.VersionedValue
-	CleanupExpired() int
-}
